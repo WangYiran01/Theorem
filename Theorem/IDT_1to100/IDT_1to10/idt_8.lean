a84ce541c1a15ec9fcdca49b7ff4feece11230a4
WangYiran01 <129300550+WangYiran01@users.noreply.github.com>
2024-05-07 12:34:52 +0800

    Delete Theorem/IDT_1to100/IDT_1to10 directory

--- Theorem/IDT_1to100/IDT_1to10/idt_8.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Data.Nat.Choose.Sum
-import Mathlib.Tactic.Linarith
--- import Mathlib.Algebra.GroupWithZero.Defs
-
-
-open Nat
-
--- theorem idt8 {n k : ℕ} (hk : k ≤ n) :
---  (n-k)*choose n k = k * choose (n-1) k  := by sorry
