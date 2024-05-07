71c7d05fa29f3979069f23105a694954539d6b58
WangYiran01 <129300550+WangYiran01@users.noreply.github.com>
2024-05-07 12:35:28 +0800

    Delete Theorem/example_separate directory

--- Theorem/example_separate/two_mul_sub.lean
+++
@@ -1,1 +0,0 @@
-theorem two_mul_sub: 2 * n - n = 2 * n - 1 * n := by simp
--- Theorem/example_separate/two_pow_eq_two_pow_two.lean
+++
@@ -1,9 +0,0 @@
-import Theorem.example_separate.add_div_two
-
-theorem two_pow_eq_two_pow_two(h: 2 ≤ n) : 2 ^ ( n - 1 ) = 2 ^ ( n - 2 ) * 2  := by
-  have sub_add_eq_sub: n - 2 + 1 = n - 1 := by
-      exact tsub_add_eq_add_tsub h
-  have two_pow_eq_two_pow_sub_add : 2 ^ ( n - 1 ) = 2 ^ ( n - 2 + 1 ) := by
-    rw[sub_add_eq_sub]
-  rw[two_pow_eq_two_pow_sub_add]
-  rw[Nat.pow_succ]
